de41d58a37e679a761e675e3ec59a42e84b887ee
George <guozhaozhong@navinfo.com>
2017-12-05 15:31:20 +0800

    Realized issue #1 and #2

--- DoomDHNode/DoomDHNode.c
+++ DoomDHNode/DoomDHNode.c
@@ -1,3 +1,4 @@
+#include<stdlib.h>
 #include<stdio.h>  
 #include<poll.h>  
 #include<netdb.h>
@@ -10,47 +11,163 @@
 #include<assert.h>
 
 #define SIZE 1024
+#define MAX_NODE 1024
+                                      
+int    fds[3];
+char   fdsip[16][3];
+int    fdsport[3];
+const int len = 3;
+char   inbuf[SIZE];
+char   outbuf[SIZE];  
 
-int    fds[2];
-const int len = 2;
+struct doom_dh_node
+{
+    int   socket;
+    char  ip[16];
+    int   port;   
+};
+struct    doom_dh_node*  psiblingnodes[MAX_NODE];
+
+struct doom_dh_node* add_sibling_node(int server)
+{
+    int j, ret;
+    char* ip;
+    char* port;
+    struct sockaddr_in nodeaddr;  
+                    
+    ip  = inbuf + strlen("addsiblingnode");
+    while(*ip == ' ') 
+        ip++;
+    port = ip;
+    while(*port != ':') port++;
+    *port = '\0';
+    port ++;
+    
+    for(j = 0; j < MAX_NODE; j++)
+    {
+        if(psiblingnodes[j] != NULL)
+        {
+            if( strcmp(psiblingnodes[j]->ip, ip) == 0 && 
+            	  atoi(port) == psiblingnodes[j]->port )
+            {
+                break;
+            }
+        }
+    }
+    if(j < MAX_NODE)
+    {
+        sprintf(outbuf, "ERR:Failed to add sibling node %s %s, node was already connected, or port was already used", ip, port);
+        send(server,outbuf, strlen(outbuf), 0);
+        return NULL;
+    }
+    
+                    
+    for(j = 0; j < MAX_NODE; j++)
+    {
+        if(psiblingnodes[j] == NULL)
+        {
+            psiblingnodes[j] = (struct doom_dh_node*)malloc(sizeof(struct doom_dh_node));
+            break;
+        }
+    }
+                    
+    if(j < MAX_NODE)
+    {
+        strcpy(psiblingnodes[j]->ip, ip);
+        psiblingnodes[j]->port = atoi(port);
+        psiblingnodes[j]->socket = socket(AF_INET,SOCK_STREAM,0);
+
+        nodeaddr.sin_family =AF_INET;  
+        nodeaddr.sin_port   = htons(psiblingnodes[j]->port);
+        nodeaddr.sin_addr.s_addr=inet_addr(ip);
+        ret = connect(psiblingnodes[j]->socket,(struct sockaddr*)&nodeaddr,sizeof(nodeaddr));
+        if(ret < 0)
+        {
+            free(psiblingnodes[j]);
+            psiblingnodes[j] = NULL;
+            sprintf(outbuf, "ERR:Failed to add sibling node, %s:%s, errno=%d\n", ip, port, errno);
+            send(server,outbuf, strlen(outbuf), 0);
+            return NULL;
+        }
+        else
+        {
+            sprintf(outbuf, "INF:New sibling node added, %s:%s\n", ip, port);
+            send(server,outbuf, strlen(outbuf), 0);
+            return psiblingnodes[j];
+        }
+        
+    }
+    else
+    {
+        sprintf(outbuf, "ERR:Failed to add sibling node, too many nodes\n");
+        send(server,outbuf, strlen(outbuf), 0);
+        return NULL;
+    }
+};
+
+
 
 int main(int argc, char* argv[])  
 {
-    int    i, ret;
-    int    ser,cli;  
-    char   inbuf[SIZE];
-    char   outbuf[SIZE];  
-    struct sockaddr_in seraddr,cliaddr;  
+    int    i, j, ret;
+    int    ser,node,cli;  
+    struct sockaddr_in seraddr,nodeaddr, cliaddr;  
     fd_set  rdfs;
-    int     port;
+    int     serverport, nodeport;
     
-    if(argc != 2)
+    if(argc != 4)
     {
-        printf("Usage: %s PORT_NUMBER\n", argv[0]);
+        printf("Usage: %s IP SERVER_PORT NODE_PORT\n", argv[0]);
         return 1;
     }
-    port = atoi(argv[1]);
     
+    
+    serverport = atoi(argv[2]);
+    nodeport   = atoi(argv[3]);
+
     socklen_t clilen        = sizeof(cliaddr);  
  
     seraddr.sin_family      = AF_INET;  
-    seraddr.sin_addr.s_addr = htonl(INADDR_ANY);  
-    seraddr.sin_port        = htons(port);  
+    //seraddr.sin_addr.s_addr = htonl(INADDR_ANY);  
+    seraddr.sin_addr.s_addr = inet_addr(argv[1]);
+    seraddr.sin_port        = htons(serverport);  
     ser    = socket(AF_INET,SOCK_STREAM,0);  
     ret    = bind(ser,(struct sockaddr*)&seraddr,sizeof(seraddr));  
 
     ret    = listen(ser,5); 
     if(ret < 0)
     {
-        printf("Failed to listen on port %d, errno = %d\n", port, errno);
+        printf("Failed to listen on port %d, errno = %d\n", serverport, errno);
+        return 1;
+    }
+
+    nodeaddr.sin_family      = AF_INET;  
+    nodeaddr.sin_addr.s_addr = htonl(INADDR_ANY);  
+    nodeaddr.sin_port        = htons(nodeport);  
+    node   = socket(AF_INET,SOCK_STREAM,0);  
+    ret    = bind(node,(struct sockaddr*)&nodeaddr,sizeof(nodeaddr));  
+
+    ret    = listen(node,5); 
+    if(ret < 0)
+    {
+        printf("Failed to listen on port %d, errno = %d\n", nodeport, errno);
         return 1;
     }
     
+    printf("Listening server on %s:%s...\n", argv[1], argv[2]);
+    printf("Listening node on %s:%s...\n", argv[1], argv[3]);
+
+    
+    for( i = 0; i < MAX_NODE; i++)
+    {
+        psiblingnodes[i] = NULL;
+    }
     for(i = 0; i < len; i++)
     {
         fds[i] = -1;
     }
     fds[0] = ser;
+    fds[1] = node;
 
     while(1)  
     {   
@@ -62,7 +179,15 @@ int main(int argc, char* argv[])
             {
                 FD_SET(fds[i], &rdfs);
             }
-            max_fd = fds[i] > max_fd?fds[i]:max_fd;
+            max_fd = fds[i] > max_fd ? fds[i] : max_fd;
+        }
+        for(i = 0; i < MAX_NODE; i++)
+        {
+            if(psiblingnodes[i] != NULL)
+            {
+                FD_SET(psiblingnodes[i]->socket, &rdfs);
+                max_fd = psiblingnodes[i]->socket > max_fd ? psiblingnodes[i]->socket : max_fd;
+            }
         }
         ret = select(max_fd + 1,&rdfs,NULL, NULL, NULL);
         if(ret < 0)  
@@ -82,21 +207,55 @@ int main(int argc, char* argv[])
                 cli=accept(ser,(struct sockaddr*)&cliaddr,&clilen);
                 if(cli > 0)
                 {
-                    printf("connect to %s:%d\n",inet_ntoa(cliaddr.sin_addr),  
-                            ntohs(cliaddr.sin_port));
                     for(i = 0; i < len; i++)
                     {
                         if(fds[i] == -1)
                         {
                             fds[i] = cli;
+                            strcpy(fdsip[i],inet_ntoa(cliaddr.sin_addr));
+                            fdsport[i] = ntohs(cliaddr.sin_port);
                             break;
                         }
                     }
                     if(i == len)
                     {
-                        sprintf(outbuf, "Too many connections\n");
+                        sprintf(outbuf, "ERR:The node was already connected to a server.\n");
                         send(cli,outbuf, strlen(outbuf), 0);
                         close(cli);
+                    }
+                    else
+                    {
+                        printf("connect to server %s:%d\n",inet_ntoa(cliaddr.sin_addr),  
+                                ntohs(cliaddr.sin_port));
+                    }
+                }
+            }
+            else if(FD_ISSET(node, &rdfs))
+            {
+                cli=accept(node,(struct sockaddr*)&cliaddr,&clilen);
+                if(cli > 0)
+                {
+                    for(i = 0; i < MAX_NODE; i++)
+                    {
+                        if(psiblingnodes[i] == NULL)
+                        {
+                            psiblingnodes[i] = (struct doom_dh_node*)malloc(sizeof(struct doom_dh_node));
+                            psiblingnodes[i]->socket = cli;
+                            strcpy(psiblingnodes[i]->ip, inet_ntoa(cliaddr.sin_addr));
+                            psiblingnodes[i]->port = ntohs(cliaddr.sin_port);
+                            break;
+                        }
+                    }
+                    if(i == MAX_NODE)
+                    {
+                        sprintf(outbuf, "ERR:Failed to connect to sibling node, too many connections\n");
+                        send(cli,outbuf, strlen(outbuf), 0);
+                        close(cli);
+                    }
+                    else
+                    {
+                        printf("connect to sibling node %s:%d\n",inet_ntoa(cliaddr.sin_addr),  
+                                ntohs(cliaddr.sin_port));
                     }
                 }
             }
@@ -106,15 +265,38 @@ int main(int argc, char* argv[])
                 {
                     if(i != 0 && FD_ISSET(fds[i], &rdfs))
                     {
+                        cli = fds[i];	
                         break;
                     }
                 }
-                assert(i != 0 && i < len);
-                ret = recv(fds[i],inbuf,SIZE,0);
+                if(i >= len)
+                {
+                    for(j = 0; j < MAX_NODE; j++)
+                    {
+                        if(psiblingnodes[j] != NULL && FD_ISSET(psiblingnodes[j]->socket, &rdfs))
+                        {
+                            cli = psiblingnodes[j]->socket;
+                            break;
+                        }
+                    }
+                    assert(j < MAX_NODE);
+                }
+                ret = recv(cli,inbuf,SIZE,0);
             	  if(ret == 0)
             	  {   
-            	      close(fds[i]);
-            	      fds[i] = -1;
+            	      close(cli);
+            	      if(i < len)
+            	      {
+            	          printf("Connection %s:%d closed!\n", fdsip[i], fdsport[i]);
+            	          fds[i] = -1;
+            	      }
+            	      else
+            	      {
+            	          printf("Connection %s:%d closed!\n", psiblingnodes[j]->ip, psiblingnodes[j]->port);
+            	          assert(j < MAX_NODE);
+            	          free(psiblingnodes[j]);
+            	          psiblingnodes[j] = NULL;
+            	      }
             	      continue;
             	  }
             	  else if(ret < 0)
@@ -125,13 +307,47 @@ int main(int argc, char* argv[])
                 //ret > 0
                 if(strcmp(inbuf, "quit\r\n") == 0)
                 {
-                    close(fds[i]);
-                    fds[i] = -1;
+            	      close(cli);
+            	      if(i < len)
+            	      {
+            	          printf("Connection %s:%d closed!\n", fdsip[i], fdsport[i]);
+            	          fds[i] = -1;
+            	      }
+            	      else
+            	      {
+            	          printf("Connection %s:%d closed!\n", psiblingnodes[j]->ip, psiblingnodes[j]->port);
+            	          assert(j < MAX_NODE);
+            	          free(psiblingnodes[j]);
+            	          psiblingnodes[j] = NULL;
+            	      }
+            	      continue;
+                }
+
+                if(strncmp(inbuf, "addsiblingnode", strlen("addsiblingnode")) == 0)
+                {
+                    struct doom_dh_node* pnode;
+                    pnode = add_sibling_node(cli);
                     continue;
                 }
+
+                j = strlen(inbuf);
+                while(j > 0 && (inbuf[j - 1] == '\r' || inbuf[j - 1] == '\n'))
+                {
+                    inbuf[j - 1] = '\0';
+                    j--;
+                }
                 printf("%s\n", inbuf);
-                //sprintf(outbuf, "Hello: %s", inbuf);
-                //send(fds[i],outbuf, strlen(outbuf), 0);
+                if(strncmp(inbuf,"TSK:", strlen("TSK:")) == 0 && strstr(inbuf, "node") == NULL)
+                {
+                    sprintf(outbuf, "%s(node: %s:%d)\n", inbuf, inet_ntoa(seraddr.sin_addr), ntohs(seraddr.sin_port));
+                    for(j = 0; j < MAX_NODE; j++)
+                    {
+                        if(psiblingnodes[j])
+                        {
+                            send(psiblingnodes[j]->socket,outbuf, strlen(outbuf), 0);
+                        }
+                    }
+                }
             }
         }
     }
--- DoomDHServer/DoomDHServer.c
+++ DoomDHServer/DoomDHServer.c
@@ -11,52 +11,150 @@
 #include<assert.h>
 
 #define SIZE     1024
-#define PORT     8274
 #define MAX_NODE 1024
 
-int    fds[5];
+int       fds[5];
+char      fdsip[16][5];
+int       fdsport[5];
 const int len = 5;
+char      inbuf[SIZE];
+char      outbuf[SIZE];  
 
-struct mood_dh_node
+struct doom_dh_node
 {
     int   socket;
     char  ip[16];
-    int   port;   
+    int   serverport;   
+    int   nodeport;
+};
+struct    doom_dh_node*  pchildnodes[MAX_NODE];
+
+struct doom_dh_node* add_child_node(int server)
+{
+    int j, ret;
+    char *ip;
+    char *serverport, *nodeport;
+    struct sockaddr_in nodeaddr;  
+
+    ip = inbuf + strlen("addchildnode");
+    while(*ip == ' ') //skip ' '
+        ip++;
+
+    serverport = ip;
+    while(*serverport != ' ') 
+        serverport++;
+    *serverport = '\0';
+    serverport++;
+    
+    nodeport = serverport + 1;
+    while(*nodeport != ' ') 
+        nodeport++;
+    *nodeport = '\0';
+    nodeport++;
+                    
+
+    for(j = 0; j < MAX_NODE; j++)
+    {
+        if(pchildnodes[j] != NULL)
+        {
+            if( strcmp(pchildnodes[j]->ip, ip) == 0 && 
+            	  atoi(serverport) == pchildnodes[j]->serverport ||
+            	  atoi(serverport) == pchildnodes[j]->nodeport ||
+            	  atoi(nodeport)   == pchildnodes[j]->serverport ||
+            	  atoi(nodeport)   == pchildnodes[j]->nodeport )
+            {
+                break;
+            }
+        }
+    }
+    if(j < MAX_NODE)
+    {
+        sprintf(outbuf, "ERR:Failed to add child node %s %s %s, node was already connected, or port was already used", ip, serverport, nodeport);
+        send(server,outbuf, strlen(outbuf), 0);
+        return NULL;
+    }
+    for(j = 0; j < MAX_NODE; j++)
+    {
+        if(pchildnodes[j] == NULL)
+        {
+            pchildnodes[j] = (struct doom_dh_node*)malloc(sizeof(struct doom_dh_node));
+            break;
+        }
+    }
+                    
+    if(j < MAX_NODE)
+    {
+        strcpy(pchildnodes[j]->ip, ip);
+        pchildnodes[j]->serverport = atoi(serverport);
+        pchildnodes[j]->nodeport   = atoi(nodeport);
+        pchildnodes[j]->socket = socket(AF_INET,SOCK_STREAM,0);
+
+        nodeaddr.sin_family =AF_INET;  
+        nodeaddr.sin_port   = htons(pchildnodes[j]->serverport);
+        nodeaddr.sin_addr.s_addr=inet_addr(ip);
+        ret = connect(pchildnodes[j]->socket,(struct sockaddr*)&nodeaddr,sizeof(nodeaddr));
+        if(ret < 0)
+        {
+            free(pchildnodes[j]);
+            pchildnodes[j] = NULL;
+            sprintf(outbuf, "ERR:Failed to add child node, %s:%s\n", ip, serverport);
+            send(server,outbuf, strlen(outbuf), 0);
+            return NULL;
+        }
+        else
+        {
+            sprintf(outbuf, "INF:New node added, %s:%s\n", ip, serverport);
+            send(server,outbuf, strlen(outbuf), 0);
+            return pchildnodes[j];
+        }
+        
+    }
+    else
+    {
+        sprintf(outbuf, "ERR:Failed to add child node, too many nodes");
+        send(server,outbuf, strlen(outbuf), 0);
+        return NULL;
+    }
 };
 
-int main()  
+int main(int argc, char* argv[])  
 {
     int    i, j, ret;
     int    ser, cli;
+    int    port;
     
-    char   inbuf[SIZE];
-    char   outbuf[SIZE];  
-    struct sockaddr_in seraddr,cliaddr, nodeaddr;  
+    struct sockaddr_in seraddr,cliaddr;  
     fd_set  rdfs;
 
-    struct mood_dh_node*  pnode[MAX_NODE];
+    if(argc != 3)
+    {
+        printf("Usage: %s IP PORT\n", argv[0]);
+        return 1;
+    }
     
+    port = atoi(argv[2]);
     
     socklen_t clilen        =sizeof(cliaddr);  
  
     seraddr.sin_family      = AF_INET;  
-    seraddr.sin_addr.s_addr = htonl(INADDR_ANY);  
-    seraddr.sin_port        = htons(PORT);  
+    //seraddr.sin_addr.s_addr = htonl(INADDR_ANY);  
+    seraddr.sin_addr.s_addr = inet_addr(argv[1]);
+    seraddr.sin_port        = htons(port);  
     ser    = socket(AF_INET,SOCK_STREAM,0);  
     ret    = bind(ser,(struct sockaddr*)&seraddr,sizeof(seraddr));
     
-    for( i = 0; i < MAX_NODE; i++)
-    {
-        pnode[i] = NULL;
-    }
-
     ret    = listen(ser,5); 
     if(ret < 0)
     {
-        printf("Failed to listen on port %d, errno = %d\n", PORT, errno);
+        printf("Failed to listen on port %d, errno = %d\n", port, errno);
         return 1;
     }
+    printf("Listening on %s:%s...\n", argv[1], argv[2]);
     
+    for( i = 0; i < MAX_NODE; i++)
+    {
+        pchildnodes[i] = NULL;
+    }
     for(i = 0; i < len; i++)
     {
         fds[i] = -1;
@@ -74,6 +172,14 @@ int main()
                 FD_SET(fds[i], &rdfs);
             }
             max_fd = fds[i] > max_fd?fds[i]:max_fd;
+        }
+        for(i = 0; i < MAX_NODE; i++)
+        {
+            if(pchildnodes[i] != NULL)
+            {
+                FD_SET(pchildnodes[i]->socket, &rdfs);
+                max_fd = pchildnodes[i]->socket > max_fd ? pchildnodes[i]->socket : max_fd;
+            }
         }
         ret = select(max_fd + 1,&rdfs,NULL, NULL, NULL);
         if(ret < 0)  
@@ -93,21 +199,27 @@ int main()
                 cli=accept(ser,(struct sockaddr*)&cliaddr,&clilen);
                 if(cli > 0)
                 {
-                    printf("connect to %s:%d\n",inet_ntoa(cliaddr.sin_addr),  
-                            ntohs(cliaddr.sin_port));
                     for(i = 0; i < len; i++)
                     {
                         if(fds[i] == -1)
                         {
                             fds[i] = cli;
+                            strcpy(fdsip[i],inet_ntoa(cliaddr.sin_addr));
+                            fdsport[i] = ntohs(cliaddr.sin_port);
                             break;
                         }
                     }
                     if(i == len)
                     {
-                        sprintf(outbuf, "Too many connections\n");
+                        sprintf(outbuf, "ERR: failed to connect to the server, too many connections\n");
                         send(cli,outbuf, strlen(outbuf), 0);
                         close(cli);
+                    }
+                    else
+                    {
+                        printf("Connected to %s:%d\n",inet_ntoa(cliaddr.sin_addr),  
+                                ntohs(cliaddr.sin_port));
+                    
                     }
                 }
             }
@@ -117,15 +229,38 @@ int main()
                 {
                     if(i != 0 && FD_ISSET(fds[i], &rdfs))
                     {
+                        cli = fds[i];	
                         break;
                     }
                 }
-                assert(i != 0 && i < len);
-                ret = recv(fds[i],inbuf,SIZE,0);
+                if(i >= len)
+                {
+                    for(j = 0; j < MAX_NODE; j++)
+                    {
+                        if(pchildnodes[j] != NULL && FD_ISSET(pchildnodes[j]->socket, &rdfs))
+                        {
+                            cli = pchildnodes[j]->socket;
+                            break;
+                        }
+                    }
+                    assert(j < MAX_NODE);
+                }
+                ret = recv(cli,inbuf,SIZE,0);
             	  if(ret == 0)
             	  {   
-            	      close(fds[i]);
-            	      fds[i] = -1;
+            	      close(cli);
+            	      if(i < len)
+            	      {
+            	          printf("Connection %s:%d closed!\n", fdsip[i], fdsport[i]);
+            	          fds[i] = -1;
+            	      }
+            	      else
+            	      {
+            	          printf("Connection %s:%d closed!\n", pchildnodes[j]->ip, pchildnodes[j]->serverport);
+            	          assert(j < MAX_NODE);
+            	          free(pchildnodes[j]);
+            	          pchildnodes[j] = NULL;
+            	      }
             	      continue;
             	  }
             	  else if(ret < 0)
@@ -136,65 +271,55 @@ int main()
                 //ret > 0
                 if(strncmp(inbuf, "quit", 4) == 0)
                 {
-                    close(fds[i]);
-                    fds[i] = -1;
+            	      close(cli);
+                    if(i < len)
+                    {
+            	          printf("Connection %s:%d closed!\n", fdsip[i], fdsport[i]);
+                        fds[i] = -1;
+                    }
+                    else
+                    {
+            	          printf("Connection %s:%d closed!\n", pchildnodes[j]->ip, pchildnodes[j]->serverport);
+            	          assert(j < MAX_NODE);
+            	          free(pchildnodes[j]);
+            	          pchildnodes[j] = NULL;
+                    }
+            	      continue;
+                }
+                
+                if(strncmp(inbuf, "addchildnode", strlen("addchildnode")) == 0)
+                {
+                    struct doom_dh_node* pnode;
+                    if((pnode = add_child_node(cli))!=NULL)
+                    {
+                        for(j = 0; j < MAX_NODE; j++)
+                        {
+                            if(pchildnodes[j] != NULL && pchildnodes[j] != pnode)
+                            {
+                                sprintf(outbuf, "addsiblingnode %s:%d\n", pnode->ip, pnode->nodeport);
+                                send(pchildnodes[j]->socket,outbuf, strlen(outbuf), 0);
+                            }
+                        }
+                    }
                     continue;
                 }
                 
-                if(strncmp(inbuf, "addnode", 7) == 0)
+                j = strlen(inbuf);
+                while(j > 0 && (inbuf[j - 1] == '\r' || inbuf[j - 1] == '\n'))
                 {
-                    char* ip  = inbuf + 7;
-                    char* port;
-                    
-                    while(*ip == ' ') ip++;
-                    port = ip;
-                    while(*port != ':') port++;
-                    *port = '\0';
-                    port ++;
-                    
+                    inbuf[j - 1] = '\0';
+                    j--;
+                }
+                printf("%s\n", inbuf);
+                if(strncmp(inbuf,"TSK:", strlen("TSK:")) == 0)
+                {
+                    sprintf(outbuf, "%s(server: %s:%d)\n", inbuf, inet_ntoa(seraddr.sin_addr), ntohs(seraddr.sin_port));
                     for(j = 0; j < MAX_NODE; j++)
                     {
-                        if(pnode[j] == NULL)
+                        if(pchildnodes[j])
                         {
-                            pnode[j] = (struct mood_dh_node*)malloc(sizeof(struct mood_dh_node));
-                            break;
+                            send(pchildnodes[j]->socket,outbuf, strlen(outbuf), 0);
                         }
-                    }
-                    
-                    if(j < MAX_NODE)
-                    {
-                        strcpy(pnode[j]->ip, ip);
-                        pnode[j]->port = atoi(port);
-                        pnode[j]->socket = socket(AF_INET,SOCK_STREAM,0);
-
-					              nodeaddr.sin_family =AF_INET;  
-	                      nodeaddr.sin_port   = htons(pnode[j]->port);
-	                      nodeaddr.sin_addr.s_addr=inet_addr(ip);
-	                      printf("\nhostname = %s, port = %s\n", ip, port);
-	                      ret = connect(pnode[j]->socket,(struct sockaddr*)&nodeaddr,sizeof(nodeaddr));
-	                      if(ret < 0)
-	                      {
-	                          free(pnode[j]);
-	                          pnode[j] = NULL;
-                            sprintf(outbuf, "Failed to add node, %s:%s", ip, port);
-            	              send(fds[i],outbuf, strlen(outbuf), 0);
-	                      }
-	                      else
-	                      {
-                            sprintf(outbuf, "New node added, %s:%s", ip, port);
-                            printf("%s", outbuf);
-            	              send(fds[i],outbuf, strlen(outbuf), 0);
-	                      }
-                    }
-                    continue;
-                }
-                sprintf(outbuf, "Hello: %s", inbuf);
-                send(fds[i],outbuf, strlen(outbuf), 0);
-                for(j = 0; j < MAX_NODE; j++)
-                {
-                    if(pnode[j])
-                    {
-                        send(pnode[j]->socket,outbuf, strlen(outbuf), 0);
                     }
                 }
             }
